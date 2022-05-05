353ec35d9a29f6e5ad37a9ff8604c0f09312f897
axdelafuen <Axel.DE_LA_FUENTE@etu.uca.fr>
2022-05-05 08:44:37 +0200

    ajout de la doc

--- Sources/VSCSolution/BibliothequeClassesVSC/Amelioration.cs
+++ Sources/VSCSolution/BibliothequeClassesVSC/Amelioration.cs
@@ -8,6 +8,15 @@ namespace BibliothequeClassesVSC
 {
     public class Amelioration : Arme
     {
+        /// <summary>
+        /// constructeur de la classe Amelioration
+        /// </summary>
+        /// <param name="nom"></param>
+        /// <param name="desc"></param>
+        /// <param name="image"></param>
+        /// <param name="niveau"></param>
+        /// <param name="active"></param>
+        /// <param name="passive"></param>
         public Amelioration(string nom, string desc = "N/A", string image = "N/A", byte niveau = 1,ArmeActive active=null,ArmePassive passive=null)
             : base(nom, desc, image, niveau) 
         {
@@ -15,10 +24,19 @@ namespace BibliothequeClassesVSC
             ArmePass = passive;
         }
 
+        /// <summary>
+        /// declaration de l'attribut ArmeAct (arme active necessaire à l'amélioration) avec son getter et setter
+        /// </summary>
         public ArmeActive ArmeAct{get;set;}
 
+        /// <summary>
+        /// declaration de l'attribut ArmePass (arme passive necessaire à l'amélioration) avec son getter et setter
+        /// </summary>
         public ArmePassive ArmePass { get; set; }
 
+        /// <summary>
+        /// méthode d'affichage qui permet d'afficher les noms de l'arme active et de l'arme passive nécessaire pour l'amélioration
+        /// </summary>
         public void affichAmelioration()
         {
             Console.WriteLine("Arme Active : "+this.ArmeAct.Nom);
--- Sources/VSCSolution/BibliothequeClassesVSC/Element.cs
+++ Sources/VSCSolution/BibliothequeClassesVSC/Element.cs
@@ -8,6 +8,12 @@ namespace BibliothequeClassesVSC
 {
     public abstract class Element
     {
+        /// <summary>
+        /// constructeur de la classe abstraite Element
+        /// </summary>
+        /// <param name="nom"></param>
+        /// <param name="desc"></param>
+        /// <param name="image"></param>
         public Element(string nom, string desc="N/A", string image="N/A")
         {
             Nom = nom;
@@ -15,15 +21,19 @@ namespace BibliothequeClassesVSC
             Image = image;
         }
 
+        /// <summary>
+        /// declaration de l'attribut Nom avec son getter et setter
+        /// </summary>
         public string Nom { get; set; }
 
+        /// <summary>
+        /// declaration de l'attribut Description avec son getter et setter
+        /// </summary>
         public string Description { get; set; }
 
+        /// <summary>
+        /// declaration de l'attribut Image (chemin d'acces vers l'image) avec getter et setter
+        /// </summary>
         public string Image { get; set; }
-
-        public override string ToString()
-        {
-            return $"{Nom} {Description}";
-        }
     }
 }
