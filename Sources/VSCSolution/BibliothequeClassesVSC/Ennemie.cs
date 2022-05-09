699dd9c310f958324127f36ec6621053f5e5e129
viastolfi <vincent.astolfi@etu.uca.fr>
2022-05-09 16:20:11 +0200

    ajout des commentaires de la classe Ennemie

--- Sources/VSCSolution/BibliothequeClassesVSC/Ennemie.cs
+++ Sources/VSCSolution/BibliothequeClassesVSC/Ennemie.cs
@@ -8,6 +8,13 @@ namespace BibliothequeClassesVSC
 {
     public class Ennemie : Entite
     {
+        /// <summary>
+        /// Constructeur de la classe Ennemie
+        /// </summary>
+        /// <param name="nom">Nom de l'ennemie que l'on souhaite créer</param>
+        /// <param name="particularite">Liste de Stat correspondant aux particularités de l'ennemie que l'on souhaite créer. Ces stats sont des améliorations des stats de bases posséder par tout les ennemies qui lui sont prores</param>
+        /// <param name="desc">Description de l'ennemie que l'on souhaite creér.</param>
+        /// <param name="image">Chemin qui permet d'accéder à l'image correspondante à l'ennemie que l'on douhaite créer.</param>
         public Ennemie(string nom, List<Stat> particularite, string desc = "N/A", string image = "N/A") : base(nom,particularite, desc, image)
         {
             AjoutParticularite(particularite);
