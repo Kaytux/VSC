b40dc3e7f489e71233341c23d212a03d626b97f9
Félix Mielcarek <felix.mielcarek@etu.uca.fr>
2022-06-09 15:03:14 +0200

    Debug converter.

--- Sources/VSCSolution/VuesVSC/converters/Func2WindowPartConverter.cs
+++ Sources/VSCSolution/VuesVSC/converters/Func2WindowPartConverter.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace VuesVSC.converters
 {
-    public class Func2WindowPartConverter : IValueConverter
+    class Func2WindowPartConverter : IValueConverter
     {
-     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             Func<UserControl> windowPartCreator = value as Func<UserControl>;
             if (windowPartCreator == null)
