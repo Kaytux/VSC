9d8eebb2415f24702659aa39928a3082623dbe17
Félix Mielcarek <felix.mielcarek@etu.uca.fr>
2022-06-09 16:29:06 +0200

    Début UC cartes

--- Sources/VSCSolution/VuesVSC/UCCartes.xaml.cs
+++ Sources/VSCSolution/VuesVSC/UCCartes.xaml.cs
@@ -1,28 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using BibliothequeClassesVSC;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VuesVSC
 {
     /// <summary>
     /// Logique d'interaction pour UCCartes.xaml
     /// </summary>
+    /// 
     public partial class UCCartes : UserControl
     {
+        public Manager Manager => (App.Current as App).Manager;
         public UCCartes()
         {
             InitializeComponent();
+            DataContext = Manager;
         }
     }
 }
